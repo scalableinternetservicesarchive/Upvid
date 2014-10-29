164203f82d16b5763826d0761e4c6ca1e849759d
Asbjørn Steinskog <asbjorn@steinskog.me>
2014-10-28 17:38:26 -0700

    Add validations for video model

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,6 @@
 class Video < ActiveRecord::Base
 	belongs_to :user
 	has_and_belongs_to_many :users
+	validates :title, presence: true, length: { maximum: 20 }
+	validates :category, presence: true, length: {maximum: 20}
 end
