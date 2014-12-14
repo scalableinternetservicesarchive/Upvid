56214f675d3da04ab94a675a3069efe1b1e8e6ae
Emil Daijiro Grønnbeck <emilgroenn@gmail.com>
2014-12-13 20:40:15 -0800

    added glyphs to project

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,8 @@ Bundler.require(*Rails.groups)
 module Upvid
   class Application < Rails::Application
     config.autoload_paths += %W(#{Rails.root}/lib/paperclip_processors)
+    config.assets.paths << "#{Rails}/vendor/assets/fonts"
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
