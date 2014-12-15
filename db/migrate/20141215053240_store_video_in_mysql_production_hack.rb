a081f0b58a9757b5c5fede6c55f7a145237ed107
Emil Daijiro Grønnbeck <emilgroenn@gmail.com>
2014-12-14 22:23:38 -0800

    test without set_packetsize

--- db/migrate/20141215053240_store_video_in_mysql_production_hack.rb
+++ db/migrate/20141215053240_store_video_in_mysql_production_hack.rb
@@ -1,7 +1,6 @@
 class StoreVideoInMysqlProductionHack < ActiveRecord::Migration
   def self.up
         execute 'ALTER TABLE local_videofiles MODIFY file_contents LONGBLOB'
-        execute 'set GLOBAL max_allowed_packet=167772160'
   end
 
 end
